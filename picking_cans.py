f681ad015542367587af3d316f3b564c11f8064c
Sam Tetruashvili <samt@alumni.cmu.edu>
2016-07-10 22:45:18 -0400

    Made a simple board class the can be printed to the command line.

--- picking_cans.py
+++ picking_cans.py
@@ -1,2 +1,35 @@
+import random
+import termcolor
+
+class Board:
+    def __init__(self, rows, columns, board=None):
+        self._rows = rows
+        self._columns = columns
+        if board is None:
+            self._board = [[False for _ in range(columns)] for _ in range(rows)]
+        else:
+            self._board = board
+
+    def Randomize(self):
+        for r in range(self._rows):
+            for c in range(self._columns):
+                self._board[r][c] = random.randint(0, 1) % 2
+
+    def ContainsCan(self, r, c):
+        return self._board[r][c]
+
+    def __str__(self):
+        output = ""
+        for r in range(self._rows):
+            for c in range(self._columns):
+                color = "on_yellow"
+                if self.ContainsCan(r, c):
+                    color = "on_red"
+                output += "|" + termcolor.colored("  ", on_color=color)
+            output += "|\n"
+        return output
+
 def main():
-    print "hello, world!"
+    board = Board(10, 10)
+    board.Randomize()
+    print board
