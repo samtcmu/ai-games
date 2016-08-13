4de80d85c716d11753429ecf5805db36f6249f0c
Sam Tetruashvili <samt@alumni.cmu.edu>
2016-08-13 13:44:26 -0400

    Updated exception message in model.py.

--- model.py
+++ model.py
@@ -1,3 +1,3 @@
 class Model:
     def ActionForCurrentPosition(self, board, r, c):
-        assert False, "Model subclasses must implement ActionForCurrentPosition."
+        raise Exception, "Model subclasses must implement ActionForCurrentPosition."
