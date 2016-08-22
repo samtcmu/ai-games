63df649e714a38abc2f03bda9d1da33a966bea0a
Sam Tetruashvili <samt@alumni.cmu.edu>
2016-08-22 18:11:51 -0400

    Added some additional comments.

--- linear_regression_test.py
+++ linear_regression_test.py
@@ -3,16 +3,17 @@ import random
 
 def LinearRegressionTest():
     expected_weights = [1.0, 2.0, 3.0]
+    noise_stdev = 5.0
     training_data = CreateTrainingData(
-        expected_weights, 1000, -100.0, 100.0, noise_stdev=5.0)
+        expected_weights, 1000, -100.0, 100.0, noise_stdev=noise_stdev)
     test_data = CreateTrainingData(
-        expected_weights, 500, -100.0, 100.0, noise_stdev=5.0)
+        expected_weights, 500, -100.0, 100.0, noise_stdev=noise_stdev)
 
     model = linear_regression.LinearRegression(len(expected_weights))
     model.RandomizeSynapticWeights(random_range=(-1000.0, 1000.0))
     model.Train(training_data,
                 learning_rate=0.0000005,
-                learning_iterations=5000,
+                learning_iterations=10000,
                 verbose=True)
 
     total_difference = 0
@@ -20,6 +21,7 @@ def LinearRegressionTest():
         total_difference += abs(model.Infer(t[0]) - t[1])
     print "average absolute difference on test data: %.3f" % (
         total_difference / len(test_data),)
+    print "noise standard deviation around linear data: %.3f" % (noise_stdev,)
     
 def CreateTrainingData(weights, num_training_examples, low, high,
                        noise_stdev=5.0):
