b6a6874798da2f8c6b218f7c4eddd716cf223f53
Sam Tetruashvili <samt@alumni.cmu.edu>
2016-09-11 23:37:05 -0400

    Simple optimization.

--- evaluate_model.py
+++ evaluate_model.py
@@ -97,12 +97,13 @@ def EvaluateModels(games=1000, board_size=(10, 10), model_dir=None,
         "actions_per_game": actions_per_game,
         "verbose": verbose,
     }
+    kwargs_items = kwargs.items()
     model_files = [os.path.join(model_dir, f) for f in os.listdir(model_dir)
                                               if f.find(".stats") == -1]
     process_pool = multiprocessing.Pool(processes=len(model_files))
     evaluation_results = process_pool.map(
         RunEvaluateModel,
-        [[("model_file", f)] + kwargs.items() for f in model_files])
+        [[("model_file", f)] + kwargs_items for f in model_files])
 
     process_pool.close()
     process_pool.join()
