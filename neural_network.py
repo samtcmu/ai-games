28a3cb0a7073190b024ddf51d7c66b5f0f303d46
Sam Tetruashvili <samt@alumni.cmu.edu>
2016-09-26 18:22:43 -0400

    Implemented backprop.

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,16 @@
 import math_util
 import random
 
+def PrintMatrix(M):
+    output = ""
+    for i in range(len(M[0])):
+        output += "  row %d:" % (i + 1,)
+        for j in range(len(M)):
+            # TODO(samt): Adjust the width based on the maximum weight.
+            output += "{0:12.8f}".format(M[j][i],) + " "
+        output += "\n"
+    return output
+
 class NeuralNetwork:
     def __init__(self, input_width, output_width, hidden_layer_widths=[]):
         self._input_width = input_width
@@ -20,14 +30,14 @@ class NeuralNetwork:
                 output += "  neuron %d:" % (i + 1,)
                 for j in range(len(self._weights[l])):
                     # TODO(samt): Adjust the width based on the maximum weight.
-                    output += "{0:7.2f}".format(self._weights[l][j][i],) + " "
+                    output += "{0:8.5f}".format(self._weights[l][j][i],) + " "
                 output += "\n"
         return output
 
     def Weight(self, l, i, j):
-        assert 1 <= l <= len(self._layer_widths)
-        assert 1 <= i <= len(self._layer_widths[i])
-        assert 0 <= j <= len(self._layer_widths[j])
+        assert 1 <= l <= len(self._layer_widths) - 1
+        assert 1 <= i <= self._layer_widths[l]
+        assert 0 <= j <= self._layer_widths[l - 1]
         return self._weights[l - 1][j][i - 1]
 
     def RandomizeWeights(self, random_range=(-1.0, 1.0)):
@@ -38,38 +48,75 @@ class NeuralNetwork:
                         random_range[0], random_range[1])
 
     def Infer(self, inputs):
-        return self._Infer(inputs)[-1]
+        return self._Infer(inputs)[-1][1:]
+
+    @staticmethod
+    def _Sigmoid(x, l, depth):
+        # Applies the Sigmoid function at all layers of the neural network
+        # besides the final layer which will simply be a linear layer (i.e.
+        # this function will just be the identity function).
+        # return math_util.Sigmoid(x) if l < (depth - 1) else x
+        return math_util.Sigmoid(x)
 
     def _Infer(self, inputs):
-        outputs = []
+        depth = len(self._weights)
         current_layer_inputs = [[-1.0] + inputs]
-        for l in range(len(self._weights)):
+        outputs = [current_layer_inputs[0]]
+        for l in range(depth):
             current_layer_ouputs = [
-                [math_util.Sigmoid(x) for x in row]
+                [NeuralNetwork._Sigmoid(x, l, depth) for x in row]
                 for row in math_util.MatrixMult(current_layer_inputs,
                                                 self._weights[l])]
-            outputs.append(current_layer_ouputs[0])
+            outputs.append([-1.0] + current_layer_ouputs[0])
             current_layer_inputs = [[-1.0] + current_layer_ouputs[0]]
-
         return outputs
 
     def Fitness(self, training_data, classifications):
         output = 0.0
         for t, c in zip(training_data, classifications):
             output += (-0.5 * math_util.VectorMagnitude(
-                       math_util.VectorDifference(t[1], c[-1]))**2)
+                       math_util.VectorDifference(t[1], c[-1][1:]))**2)
         return output
 
     def _WeightsGradientForSingleTrainingExample(self, t, c, verbose=False):
         weights_gradient = [[[0.0 for i in range(self._layer_widths[l])]
                                   for j in range(self._layer_widths[l - 1] + 1)]
                                   for l in range(1, len(self._layer_widths))]
+
+        for l in range(len(self._layer_widths) - 1, 0, -1):
+            for i in range(1, self._layer_widths[l] + 1):
+                common_weight_gradient = 0.0
+                if l == len(self._layer_widths) - 1:
+                    common_weight_gradient = (
+                        (t[1][i - 1] - c[l][i]) * c[l][i] * (1.0 - c[l][i]))
+                if l < len(self._layer_widths) - 1:
+                    common_weight_gradient = 0.0
+                    for k in range(1, self._layer_widths[l + 1] + 1):
+                        common_weight_gradient += (
+                            self.Weight(l + 1, k, i) *
+                            weights_gradient[l][i][k - 1])
+                    common_weight_gradient *= (1.0 - c[l][i])
+
+                for j in range(self._layer_widths[l - 1] + 1):
+                    weights_gradient[l - 1][j][i - 1] = (
+                        common_weight_gradient * c[l - 1][j])
+
         return weights_gradient
 
     def _WeightsGradient(self, training_data, classifications, verbose=False):
         weights_gradient = [[[0.0 for i in range(self._layer_widths[l])]
                                   for j in range(self._layer_widths[l - 1] + 1)]
                                   for l in range(1, len(self._layer_widths))]
+
+        for t, c in zip(training_data, classifications):
+            weights_gradient_for_training_example = (
+                self._WeightsGradientForSingleTrainingExample(
+                    t, c, verbose=verbose))
+            for l in range(len(self._weights)):
+                weights_gradient[l] = math_util.MatrixSum(
+                    weights_gradient[l],
+                    weights_gradient_for_training_example[l])
+
         return weights_gradient
 
     def Train(self, training_data, learning_rate=1.0, learning_iterations=10,
@@ -82,20 +129,15 @@ class NeuralNetwork:
             if verbose:
                 current_fitness = self.Fitness(training_data,
                                                current_classifications)
-                print "fitness(%4d): %0.3f" % (k, current_fitness)
-                print "model(%4d): %s" % (k, self)
+                print "fitness(%4d): %s" % (k, "{:,.4f}".format(current_fitness))
+                print "model(%4d): \n%s" % (k, self)
 
             for t, c in zip(training_data, current_classifications):
+                # TODO(samt): Since we are running stochastic gradient descent
+                # here we should call _WeightsGradientForSingleTrainingExample.
                 weights_gradient = self._WeightsGradient(
                     [t], [c], verbose=False)
-                print "training example:"
-                for row in t:
-                    print "  %s" % (row,)
 
-                print "current classification:"
-                for row in c:
-                    print "  %s" % (row,)
-                print
                 for l in range(len(self._weights)):
                     self._weights[l] = math_util.MatrixSum(
                         self._weights[l],
--- neural_network_test.py
+++ neural_network_test.py
@@ -5,19 +5,19 @@ import random
 
 def NeuralNetworkTest():
     expected_weights = [200.0, 10.0, 200.0]
-    noise_stdev = 5.0
+    noise_stdev = 0.01
     training_data = CreateTrainingData(
-        expected_weights, 1000, -100.0, 100.0, noise_stdev=noise_stdev)
+        expected_weights, 1000, -20.0, 40.0, noise_stdev=noise_stdev)
     test_data = CreateTrainingData(
-        expected_weights, 500, -100.0, 100.0, noise_stdev=noise_stdev)
+        expected_weights, 500, -20.0, 40.0, noise_stdev=noise_stdev)
 
     model = neural_network.NeuralNetwork(input_width=len(expected_weights) - 1,
                                          output_width=1,
-                                         hidden_layer_widths=[4, 2])
+                                         hidden_layer_widths=[4, 8, 4, 2])
     model.RandomizeWeights(random_range=(-1.0, 1.0))
     model.Train(training_data,
-                learning_rate=0.0000005,
-                learning_iterations=10000,
+                learning_rate=0.005,
+                learning_iterations=1000,
                 regularization_rate=0.0,
                 verbose=True)
 
@@ -34,7 +34,7 @@ def CreateTrainingData(weights, num_training_examples, low, high,
     for i in range(num_training_examples):
         input_data = [-1.0] + RandomVector(len(weights) - 1, low, high)
         expected_output = (
-            math_util.VectorDotProduct(input_data, weights) +
+            math_util.Sigmoid(math_util.VectorDotProduct(input_data, weights)) +
             random.gauss(0.0, noise_stdev))
         training_data.append([input_data[1:], [expected_output]])
     return training_data
