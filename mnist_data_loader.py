b9612af1b0f0d6d08dbcd5fd4c4f1d28ab6c9e8b
Sam Tetruashvili <samt@alumni.cmu.edu>
2016-09-29 00:19:45 -0400

    Updated loading of MNIST data.

--- mnist_data_loader.py
+++ mnist_data_loader.py
@@ -14,48 +14,13 @@ MNIST_TEST_FILES = [
     "data/mnist/t10k-labels-idx1-ubyte.gz",
 ]
 
-def ProcessMnistData(training_data_path="output/mnist/training-data.dat",
-                     test_data_path="output/mnist/test-data.dat",
-                     verbose=False):
+def MnistData(verbose=False):
     training_data = ReadMnistData(
         MNIST_TRAINING_FILES[0], MNIST_TRAINING_FILES[1], verbose=verbose)
     test_data = ReadMnistData(
         MNIST_TEST_FILES[0], MNIST_TEST_FILES[1], verbose=verbose)
 
-    with open(training_data_path, "w") as training_data_file:
-        print "Saving training data to: %s" % (training_data_path,)
-        pickle.dump(training_data, training_data_file)
-
-    with open(test_data_path, "w") as test_data_file:
-        print "Saving test data to: %s" % (test_data_path,)
-        pickle.dump(test_data, test_data_file)
-
-def ProcessedMnistData(training_data_path="output/mnist/training-data.dat",
-                       test_data_path="output/mnist/test-data.dat"):
-    training_data = None
-    with open(training_data_path, "w") as training_data_file:
-        training_data = pickle.load(training_data_file)
-
-    test_data = None
-    with open(test_data_path, "w") as test_data_file:
-        test_data = pickle.load(test_data_file)
-
     return (training_data, test_data)
-
-def PrintMnistImage(image):
-    output = ""
-    for r in range(len(image)):
-        for c in range(len(image[r])):
-            color = None
-            if 0 <= image[r][c] < 64:
-                color = "on_white"
-            elif 64 <= image[r][c] < 192:
-                color = "on_yellow"
-            else:
-                color = "on_red"
-            output += termcolor.colored("  ", on_color=color)
-        output += "\n"
-    return output
 
 def ReadMnistData(image_file_path, label_file_path, verbose=False):
     data = []
@@ -113,3 +78,25 @@ def ReadMnistData(image_file_path, label_file_path, verbose=False):
         print "]\n"
 
     return data
+
+def PrintTrainingData(indices, verbose=False):
+    training_data, test_data = MnistData(verbose=verbose)
+
+    for i in indices:
+        print "training example %d:\n%s" % (i, MnistImageAsString(training_data[i][0]))
+        print "excpeted label: %d" % (training_data[i][1],)
+
+def MnistImageAsString(image):
+    output = ""
+    for r in range(len(image)):
+        for c in range(len(image[r])):
+            color = None
+            if 0 <= image[r][c] < 64:
+                color = "on_white"
+            elif 64 <= image[r][c] < 192:
+                color = "on_yellow"
+            else:
+                color = "on_red"
+            output += termcolor.colored("  ", on_color=color)
+        output += "\n"
+    return output
